5775844684ed2a6d12738ac30a4ec44f429fd49f
dhanu1411 <dhanush142002@gmail.com>
2026-05-30 14:42:08 +0530

    Added append data functionality

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-from openpyxl import Workbook
+from openpyxl import Workbook, load_workbook
+import os
 
-# Create Excel workbook
-workbook = Workbook()
-sheet = workbook.active
+filename = "student_results.xlsx"
 
-# Heading row
-sheet.append([
-    "Name",
-    "Tamil",
-    "English",
-    "Maths",
-    "Science",
-    "Total",
-    "Average",
-    "Grade"
-])
+# Check if file already exists
+if os.path.exists(filename):
+    workbook = load_workbook(filename)
+    sheet = workbook.active
+else:
+    workbook = Workbook()
+    sheet = workbook.active
+
+    # Create heading row only once
+    sheet.append([
+        "Name",
+        "Tamil",
+        "English",
+        "Maths",
+        "Science",
+        "Total",
+        "Average",
+        "Grade"
+    ])
 
 # Number of students
 num = int(input("Enter number of students: "))
@@ -33,7 +40,6 @@ for i in range(num):
     total = tamil + english + maths + science
     average = total / 4
 
-    # Grade calculation
     if average >= 90:
         grade = "A+"
     elif average >= 75:
@@ -45,7 +51,6 @@ for i in range(num):
     else:
         grade = "Fail"
 
-    # Save data into Excel
     sheet.append([
         name,
         tamil,
@@ -57,7 +62,6 @@ for i in range(num):
         grade
     ])
 
-# Save Excel file
-workbook.save("student_results.xlsx")
+workbook.save(filename)
 
-print("\nData saved successfully into student_results.xlsx")
+print("\nData added successfully!")
